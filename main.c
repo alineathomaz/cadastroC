63bcd5c8f691879872a5587c4b761e5f35b933fd
Aline Takai <99260756+alineathomaz@users.noreply.github.com>
2022-08-26 15:58:07 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	char nome [30];
+	char end [30];
+	char cidade [20];
+	int idade;
+	
+	printf ("Nome \n");
+	scanf ("%s",&nome);
+	printf ("Endereco \n");
+	scanf ("%s",&end);
+	printf ("Cidade \n");
+	scanf ("%s",&cidade);
+	printf ("Idade \n");
+	scanf ("%d",&idade);
+	
+	printf("\n Nome: %s",&nome);
+	printf("\n Endereco: %s",end);
+	printf("\n Cidade: %s",cidade);
+	printf("\n Idade: %d",idade);
+	
+	rerturn=0
+	
+	
+	
+	}
